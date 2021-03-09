0fb094d37e37220bad2e0f9a2daa076c0305276d
ashkola <ashkola@gmail.com>
2021-03-09 03:05:16 +0300

    Init

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import com.example.grpc.GreetingServiceGrpc;
+import com.example.grpc.GreetingServiceOuterClass;
+
+import java.nio.channels.Channel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    }
+}
